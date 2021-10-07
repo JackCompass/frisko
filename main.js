97ed1ad533f485ad404cd88986ae34410b0ec282
Anuj Singh <kumaranuj1303@gmail.com>
2021-10-08 02:01:02 +0530

    Add doughnut chart and bar chart along with animated background.

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 let ctx = document.getElementById('myChart').getContext("2d");
 let myChart = null;
-const drawVisuals =  (duration) => {
+const drawVisuals =  (duration, chart_type) => {
     let stackoverflow_count = 0;
     let github_count = 0;
     let youtube_count = 0;
     let search_count = 0;
-    // destroying if any chart is present.
 
     chrome.history.search({text: 'stackoverflow', startTime:Date.now() - (86400000 * duration), maxResults: 100000}, (data) => {
         stackoverflow_count = data.length;
@@ -16,11 +15,12 @@ const drawVisuals =  (duration) => {
                 chrome.history.search({text: '', startTime:Date.now() - (86400000 * duration), maxResults: 100000}, (data) => {
                     search_count = data.length - stackoverflow_count - github_count - youtube_count;
                     myChart = new Chart(ctx, {
-                        type: "doughnut",
+                        type: chart_type,
                         data: {
                             labels:["Stackoverflow", "GitHub", "YouTube", "Other"],
                             datasets:[{
                                 data: [stackoverflow_count, github_count, youtube_count, search_count],
+                                label: "Activity",
                                 backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)', 'rgb(75, 192, 192)']
                             }],
                         },
@@ -28,6 +28,17 @@ const drawVisuals =  (duration) => {
                             animation: {
                                 animateScale: true,
                             },
+                            plugins: {
+                                legend: {
+                                    display: true,
+                                    position: "top",
+                                    labels: {
+                                        pointStyle: "triangle",
+                                        usePointStyle: true,
+                                        color: "black",
+                                    }
+                                }
+                            }
                         }
                     });
                     console.log("Chart is : ", myChart);
@@ -38,12 +49,18 @@ const drawVisuals =  (duration) => {
 }
 
 
-document.getElementById('duration').addEventListener("change", (event) => {
-    if (myChart != null)
-        myChart.destroy();
-    const duration = parseInt(event.target.value, 10);
-    if (!isNaN(duration))
-        drawVisuals(event.target.value);
+const elements = document.querySelectorAll('.toggle');
+elements.forEach( (item, index) => {
+   item.addEventListener('change', (event) => {
+       if (myChart != null)
+           myChart.destroy();
+       const duration = parseInt(document.getElementById('duration').value, 10);
+       let chart_type = document.getElementById('chart-type').value;
+       console.log(chart_type);
+       if (!isNaN(duration)){
+           drawVisuals(duration, chart_type)
+       }
+   })
+});
 
-})
 
